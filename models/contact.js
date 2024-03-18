4ba1746ff77a4153f5c9490314cea8ba97391f91
Sudhanshu Sharma <93372615+Sudhanshu894@users.noreply.github.com>
2024-03-18 10:48:10 +0000

    .

--- models/contact.js
+++ models/contact.js
@@ -21,6 +21,8 @@ const contactSchema = new mongoose.Schema({
     default: true,
     required: true,
   },
+},{
+  timestamps: true
 });
 
 const Contact = mongoose.model('Contact', contactSchema);
